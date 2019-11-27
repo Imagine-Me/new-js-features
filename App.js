91666920c875c4fa924dcc5eb0dc7c99c9647182
Prince Thomas <prince.thomas@keleno.com>
2019-11-27 13:29:29 +0530

    let and const concept completd

--- App.js
+++ App.js
@@ -2,3 +2,14 @@
 // install node from following link https://nodejs.org/en/download/
 // node node in system variables
 
+let string = "Change me";//this can be changed
+console.log(string);
+
+string = "Changed";
+console.log(string); 
+
+const string1 = "Don't change me";//this can't be changed. this will throw an error
+console.log(string1);
+
+string1 = "Trying to change";
+console.log(string1);
